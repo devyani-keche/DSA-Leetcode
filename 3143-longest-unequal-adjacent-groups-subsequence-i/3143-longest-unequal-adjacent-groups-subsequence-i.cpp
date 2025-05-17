9fbd2bd3571d88d6f76fc805c63fe8503c6a621b
devyani-keche <devyanik2006@gmail.com>
2025-05-18 00:30:46 +0530

    Time: 0 ms (100%), Space: 30 MB (36.26%) - LeetHub

---
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> arr;
+        int zerocnt=0, onecnt=0,l, len1=0,len2=0;
+       
+        //count 1 and 0
+        for (int i =0;i<groups.size();i++){
+           if(groups[i]==0) zerocnt++;
+           else onecnt++;
+        }
+
+        // if(zerocnt>=onecnt){
+
+            for (int i =0;i<groups.size();i++){
+                if(groups[i]==0) arr.push_back(words[i]);
+                l=i;
+                break;
+            }
+            for (int j =l+1;j<groups.size();j++){
+                if(groups[j]!=groups[j-1]) arr.push_back(words[j]);
+            }
+            len1 = arr.size();
+            //0first
+            arr.clear();
+            for (int i =0;i<groups.size();i++){
+                if(groups[i]==1) arr.push_back(words[i]);
+                l=i;
+                break;
+            }
+            for (int j =l+1;j<groups.size();j++){
+                if(groups[j]!=groups[j-1]) arr.push_back(words[j]);
+            }
+            len2 = arr.size();
+            //1first
+            if(len2>=len1) return arr;
+            else{
+                arr.clear();
+                for (int i =0;i<groups.size();i++){
+                if(groups[i]==0) arr.push_back(words[i]);
+                l=i;
+                break;
+                }
+                for (int j =l+1;j<groups.size();j++){
+                    if(groups[j]!=groups[j-1]) arr.push_back(words[j]);
+                }
+                return arr;
+            }
+        // }
+    }
+};
