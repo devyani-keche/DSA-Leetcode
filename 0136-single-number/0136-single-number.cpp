0d03e6c06e02f4d4a34150e2f890f4d93970fa01
devyani-keche <devyanik2006@gmail.com>
2025-05-24 23:31:21 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.84%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    int singleNumber(std::vector<int>& nums) {
-        int i = 0;
-
-        while (nums.size() > 1) {
-            int j = nums.size() - 1;
-
-            while (j > i) {
-                if (nums[i] == nums[j]) {
-                    nums.erase(nums.begin() + j);
-                    nums.erase(nums.begin() + i);
-                    i = 0;
-                    break; 
-                }
-                j--;
-            }
-
-            if (j == i) {
-                i++; 
-            }
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(int i =0;i<nums.size();i++){
+            res^=nums[i];
         }
-
-
-        return nums[0];
+        return res;
     }
 };
