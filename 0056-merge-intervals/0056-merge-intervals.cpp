0e157ccd2ffe72b93feaf579dd1b23e5a03d5f04
devyani-keche <devyanik2006@gmail.com>
2025-06-02 16:28:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (59.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,27 +1,3 @@
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//         vector<vector<int>> arr;
-//         sort(intervals.begin(),intervals.end());
-//         for(int i=0;i<intervals.size();i++){
-//             if(i+1<intervals.size() && intervals[i+1][0]<=intervals[i][1] ) {
-//                 arr.push_back({intervals[i][0], max(intervals[i+1][1],intervals[i][1])});
-                
-//                 intervals[i+1][0]=intervals[i][0];
-//                 intervals[i+1][1]=max(intervals[i+1][1],intervals[i][1]);
-//             }
-//             else arr.push_back(intervals[i]);
-
-//         }
-//         int i=0;
-//         while(i+1<arr.size()){
-//             if(arr[i+1] == arr[i]) arr.erase(arr.begin()+i+1);
-            
-//             else i++;
-//         }
-//         return arr;
-//     }
-// };
 
 class Solution {
 public:
@@ -34,15 +10,13 @@ public:
         merged.push_back(intervals[0]);
         
         for (int i = 1; i < intervals.size(); ++i) {
-            // Get the last interval in the merged list
+           
             vector<int>& last = merged.back();
             
-            // Check for overlap
             if (intervals[i][0] <= last[1]) {
-                // Merge the intervals
+             
                 last[1] = max(last[1], intervals[i][1]);
             } else {
-                // No overlap, add the interval to the result
                 merged.push_back(intervals[i]);
             }
         }
