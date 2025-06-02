8fec221b7d6a6fe00c0e369dccfaa3ed02c1c151
devyani-keche <devyanik2006@gmail.com>
2025-06-02 16:04:45 +0530

    Time: 3 ms (27.13%), Space: 23 MB (43.5%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.size()==1) return 1;
+        vector<int> candy(ratings.size(),1);
+        int sum=0;
+        int a=1;
+            if(ratings[ratings.size()-2]<ratings[ratings.size()-1]) candy[ratings.size()-1]+=1;//112
+        for(int i=ratings.size()-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]) {
+                candy[i-1]=candy[i]+1;
+                // a++;
+            }
+            // else  a=1;
+            
+        }//112
+        a=1;
+        if(ratings[0]>ratings[1] && candy [0]<=candy[1]) candy[0]+=1;
+        for(int i=0;i<ratings.size()-1;i++){
+            if(ratings[i+1]>ratings[i] && candy[i+1]<=candy[i]) {
+                candy[i+1]=candy[i]+1;
+                // 123
+            }
+            // else if(ratings[i+1]==ratings[i]) a=1;
+
+        }//1231321
+         for(int i=0;i<ratings.size();i++){
+            sum+=candy[i];
+         }
+         return sum;
+    }
+};
