8a631331f94d5ac03f7f93456251c798975104b7
Devyani Keche <devyanik2006@gmail.com>
2025-06-30 22:30:02 +0530

    Time: 12 ms (81.05%), Space: 36.3 MB (72.12%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -3,19 +3,19 @@ public:
     int findLHS(vector<int>& nums) {
         if (nums.size() <= 1) return 0;
 
-        sort(nums.begin(), nums.end()); // Sort the array
+        sort(nums.begin(), nums.end()); 
         int i = 0, j = 0, maxi = 0;
 
         while (j < nums.size()) {
-            // Check if the current window is valid
+           
             if (nums[j] - nums[i] <= 1) {
                 if (nums[j] - nums[i] == 1) {
-                    // Only update maxi for harmonious subsequences
+                   
                     maxi = max(maxi, j - i + 1);
                 }
-                j++; // Expand the window
+                j++; 
             } else {
-                i++; // Shrink the window
+                i++; 
             }
         }
         
