113b5de56fe0e78bb862520df04548aa5037d5d3
Devyani Keche <devyanik2006@gmail.com>
2025-08-22 23:45:49 +0530

    Time: 245 ms (87.86%), Space: 133.6 MB (38.34%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    static int minimumArea(vector<vector<int>>& grid) {
-        const int n=grid.size(), m=grid[0].size();
-        int iMin=1000, iMax=-1, jMin=1000, jMax=-1;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==0) continue;
-                iMin=min(iMin, i);
-                iMax=max(iMax, i);
-                jMin=min(jMin, j);
-                jMax=max(jMax, j);
+    int minimumArea(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int minRow = m, maxRow = -1;
+        int minCol = n, maxCol = -1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = min(minRow, i);
+                    maxRow = max(maxRow, i);
+                    minCol = min(minCol, j);
+                    maxCol = max(maxCol, j);
+                }
             }
         }
-        return (iMax-iMin+1)*(jMax-jMin+1);
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
     }
 };
-
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
