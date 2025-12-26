fc590ad86bd70c8b71b62b0612cd38e792cc8004
Devyani Keche <devyanik2006@gmail.com>
2025-12-26 11:27:40 +0530

    Time: 3 ms (89.07%), Space: 13.2 MB (98.36%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int penalty = 0;
-        for (char c : customers)
-            if (c == 'Y') penalty++;
+        for (char c : customers) if (c == 'Y') penalty++;
         
         int minPenalty = penalty;
         int hour = 0;
 
         for(int i =0;i<customers.size();i++){
-            if(customers[i]=='Y') {
-                penalty--;
-            }else {
-                penalty++;
-            }
-             if (penalty < minPenalty) {
+            if(customers[i]=='Y') penalty--;
+            else penalty++;
+            if (penalty < minPenalty) {
                 minPenalty = penalty;
                 hour = i + 1;
             }
