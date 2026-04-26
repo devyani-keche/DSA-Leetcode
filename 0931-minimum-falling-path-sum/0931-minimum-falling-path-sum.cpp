9483dc1838959d93844481f8d01295729746e55a
Devyani Keche <devyanik2006@gmail.com>
2026-04-26 20:06:22 +0530

    Time: 0 ms (100%), Space: 14.4 MB (90.41%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,19 +4,18 @@ public:
         int n= matrix.size();
         vector<int> dp=matrix[0];
         vector<int>curr=dp;
-        int sum1=INT_MAX,sum2=INT_MAX,sum3=INT_MAX;
+        int sum1,sum2,sum3;
         for(int i =1;i<n;i++){
-            dp=curr;
             for(int j=0;j<n;j++){
                 if(j>0)sum1=dp[j-1];
                 else sum1=INT_MAX;
                 sum2=dp[j];
                 if(j<n-1)sum3=dp[j+1];
                 else sum3=INT_MAX;
-                int a = min(sum1,sum2);
-                curr[j]=matrix[i][j]+min(a,sum3);
+                curr[j]=matrix[i][j]+min({sum1,sum2,sum3});
             }
+            dp=curr;
         }
-        return *min_element(curr.begin(),curr.end());        
+        return *min_element(dp.begin(),dp.end());        
     }
 };
